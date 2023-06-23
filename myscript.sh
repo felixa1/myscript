079e9c13c00bdc404636149f7a4da43fb06be0e8
felixa1 <107949309+felixa1@users.noreply.github.com>
2023-06-23 16:41:35 +0100

    Add files via upload

---
+++ myscript.sh
@@ -0,0 +1,44 @@
+#!/bin/bash 
+
+# ECHO  COMMAND
+# echo hello world!
+
+# VARIABLES
+# UPPERCASE BY CONVENTIONS
+ NAME="Johnson Ladi"
+# FRUIT="Pawpaw, Apple $ mango"
+# echo "my name is $NAME"
+# echo "my age is $AGE"
+# USER INPUT
+# read -p "Enter your name; " NAME 
+# echo "hello $NAME, good to meet you!" 
+# echo "$NAME, what kind of $FRUIT are you giving your guest you have in house"
+
+# simple if statement
+# if [ "$NAME" == "Felix Olusegun" ]
+# then
+  #  echo "your name is Felix Olusegun"
+# sfi 
+
+# If-ELSE
+# if [ "$NAME" == "Felix Olusegun" ]
+# then
+ #   echo "your name is Felix Olusegun"
+# else
+ #   echo "your name is not Felix Olusegun"
+# fi 
+
+
+# ELSE-IF(elif)
+if [ "$NAME" == "Felix Olusegun" ]
+then
+    echo "your name is Felix Olusegun"
+elif 
+    [ "$NAME" == "Johnson Ladi" ]
+then
+    echo "your name is Not Felix Olusegun but Johnson Ladi"
+else
+    [ "$NAME" == "Johnson Ladi" ]
+fi 
+
+
